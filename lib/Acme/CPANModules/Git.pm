5a6e23e61ba3e799fe7a5c7d03d649a9d3b897e1
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-01-15 09:11:35 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/Git.pm
@@ -0,0 +1,25 @@
+package Acme::CPANModules::Git;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Modules related to the git scm tool",
+    description => <<'_',
+
+
+_
+    entries => [
+        {module=>'Text::Gitignore'},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+
+=head1 prepend:SEE ALSO
+
+L<https://orgmode.org>
